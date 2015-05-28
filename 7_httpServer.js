8b3095b5e4b42236aa5c9ceba6b5302f574bcafd
NeoNeoHo <b95042@gmail.com>
2015-05-28 11:08:50 +0800

    add no.7

--- 7_httpServer.js
+++ 7_httpServer.js
@@ -2,16 +2,21 @@ var http = require('http');
 var fs = require('fs');
 var through = require('through2');
 
-var write = function write(buf, encoding, next) {
+var write2Upper = function write2Upper(buf, encoding, next) {
 	this.push(buf.toString().toUpperCase());
 	next();
 };
 
+var end = function end(done) {
+	done();
+};
+
 var server = http.createServer(function(req, res) {
 	if (req.method === 'POST') {
-		req.pipe(through());
+		req.pipe(through(write2Upper))
+			.pipe(res);
 	}
-	res.end('beep boop\n');
+	else res.end('beep boop\n');
 });
 
-server.listen(process.argv[2]);
+server.listen(parseInt(process.argv[2]));
