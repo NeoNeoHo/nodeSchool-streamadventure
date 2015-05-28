36df37e99aee67e5097ae1092ecb1c06e7c33755
NeoNeoHo <b95042@gmail.com>
2015-05-29 01:40:16 +0800

    do not know why

--- 10_httpStream.js
+++ 10_httpStream.js
@@ -2,10 +2,19 @@ var trumpet = require('trumpet');
 var through = require('through2');
 var tr = trumpet();
 
-process.stdin.pipe(tr);
-tr.selectAll('.loud', function(elem) {
-	elem.createStream().pipe(through(function(buf, enc, next) {
-		this.push(buf.toString().toUpperCase() + '\n');
-		next();
-	})).pipe(process.stdout);
-});
+
+// var loud = tr.selectAll('.loud', function(elem) {
+// 	elem.createStream().pipe(through(function(buf, enc, next) {
+// 		this.push(buf.toString().toUpperCase() + '\n');
+// 		next();
+// 	})).pipe(elem);
+// });
+// process.stdin.pipe(tr).pipe(process.stdout);
+
+var loud = tr.select('.loud').createStream();
+loud.pipe(through(function (buf, _, next) {
+  this.push(buf.toString().toUpperCase());
+  next();
+})).pipe(loud);
+
+process.stdin.pipe(tr).pipe(process.stdout);
